cabdb17afb4085e5fda5c1548b14d441273b387d
Arturoo0 <arturoportelles@gmail.com>
2021-09-19 13:14:36 -0400

    Center login component on page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,9 +2,18 @@
 import React from 'react';
 import { LoginForm } from '../components';
 
+const loginContainer = {
+    display: 'flex',
+    height: '100vh',
+    justifyContent: 'center',
+    alignItems: 'center'
+};
+
 const Login = () => {
     return (
-        <LoginForm />
+        <div style={loginContainer}>
+            <LoginForm />
+        </div>
     );
 }; 
 
