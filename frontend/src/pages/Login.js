3ede580460befdd5cbde16e5e212f0b6a6b9c5e8
Arturoo0 <arturoportelles@gmail.com>
2021-09-19 21:57:23 -0400

    Switch form on selected type

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,19 +1,43 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 import { LoginForm, SignupForm } from '../components';
 
-const loginContainer = {
+const authContainerStyle = {
     display: 'flex',
     height: '100vh',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    flexDirection: 'column'
+};
+
+const switchFormButtonStyle = {
+    border: 'none',
+    backgroundColor: 'transparent',
+    fontSize: '1rem'
 };
 
 const Login = () => {
+    const [selectedOption, setSelectedOption] = useState('login');
+
+    const renderAuthContainer = () => {
+        if (selectedOption === 'login') return <LoginForm />
+        else return <SignupForm />
+    };  
+
     return (
-        <div style={loginContainer}>
-            <LoginForm />
-            <SignupForm />
+        <div style={authContainerStyle}>     
+            <div>
+                <button style={
+                    {
+                        ...switchFormButtonStyle,
+                        borderRight: '2px solid #ddd'
+                    }
+                } onClick={() => setSelectedOption('login')}>Login</button>
+                <button style={switchFormButtonStyle} onClick={() => setSelectedOption('signup')}>Signup</button>
+            </div>
+            <div>
+                {renderAuthContainer()}
+            </div>
         </div>
     );
 }; 
