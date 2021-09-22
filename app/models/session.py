fcb6b7c4a5ab8c6225cb56692409e76e11525dac
Arturoo0 <arturoportelles@gmail.com>
2021-09-21 21:04:18 -0400

    Store user identifier in session/user collections

--- app/models/session.py
+++ app/models/session.py
@@ -1,12 +1,14 @@
 
 class Session:
     collection_name = 'sessions'
-    def __init__(self, session_id, expiry):
+    def __init__(self, session_id, expiry, user_data_identifier):
         self.session_id = session_id
         self.expiry = expiry
+        self.user_data_identifier = user_data_identifier
     
     def generate_schema_dict(self):
         return {
             'session_id': self.session_id,
-            'expiry': self.expiry
+            'expiry': self.expiry,
+            'user_data_identifier': self.user_data_identifier
         }
--- app/models/user.py
+++ app/models/user.py
@@ -5,11 +5,12 @@ class User:
     value_error_mapper = {
         'emailErr' : 'Provided email is not a valid shape'
     }
-    def __init__(self, email, username, password):
+    def __init__(self, email, username, password, user_data_identifier):
         self.validate_email_shape(email)
         self.email = email
         self.username = username
         self.password = password
+        self.user_data_identifier = user_data_identifier
 
     def validate_email_shape(self, email):
         pattern = re.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
@@ -21,5 +22,6 @@ class User:
         return {
             'email': self.email,
             'username': self.username,
-            'password': self.password
+            'password': self.password,
+            'user_data_identifier': self.user_data_identifier
         }
