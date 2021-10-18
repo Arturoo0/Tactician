c0c7d14f3f3743d072c74c49f73c8cff17f9517f
Arturoo0 <arturoportelles@gmail.com>
2021-10-18 19:11:03 -0400

    Update csv read

--- app/puzzles/util.py
+++ app/puzzles/util.py
@@ -1,4 +1,20 @@
+import csv 
 
 class PuzzleContainer:
     def __init__(self):
+        self.id_mapped_puzzles = self.convert_csv_to_dict()
         self.puzzles_by_rating = []
+        self.convert_csv_to_dict()
+
+    def convert_csv_to_dict(self):
+        try:
+            id_mapped_puzzles = {}
+            with open('app/puzzles/lichess_db_puzzle.csv') as f:
+                reader = csv.DictReader(f)
+                for row in reader:
+                    id_mapped_puzzles[row['PuzzleId']] = row
+            return id_mapped_puzzles
+        except:
+            print('Something occured parsing the lichess CSV file.')
+
+            
