989cd612c20ad2dcb5eb00e92188a9b7b5b49ae2
Arturoo0 <arturoportelles@gmail.com>
2021-10-18 19:50:32 -0400

    Fetch puzzle based on user

--- app/puzzles/util.py
+++ app/puzzles/util.py
@@ -1,20 +1,27 @@
 import csv 
+import random
 
 class PuzzleContainer:
     def __init__(self):
-        self.id_mapped_puzzles = self.convert_csv_to_dict()
-        self.puzzles_by_rating = []
-        self.convert_csv_to_dict()
+        mapped_by_id, mapped_by_rating = self.convert_csv_to_dict()
+        self.id_mapped_puzzles = mapped_by_id
+        self.puzzles_by_rating = mapped_by_rating
 
     def convert_csv_to_dict(self):
-        try:
-            id_mapped_puzzles = {}
-            with open('app/puzzles/lichess_db_puzzle.csv') as f:
-                reader = csv.DictReader(f)
-                for row in reader:
-                    id_mapped_puzzles[row['PuzzleId']] = row
-            return id_mapped_puzzles
-        except:
-            print('Something occured parsing the lichess CSV file.')
+        id_mapped_puzzles = {}
+        puzzles_by_rating = [[] for arr in range(100)]
+        with open('app/puzzles/lichess_db_puzzle.csv') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                rating = int(row['Rating'])
+                rounded_rating = rating - (rating % 100)
+                id_mapped_puzzles[row['PuzzleId']] = row
+                puzzles_by_rating[rounded_rating//100].append(row)
+        return (id_mapped_puzzles, puzzles_by_rating)
+
+    def pull_tactic(self, rating):
+        normalized_rating = int((rating - (rating % 100))/100)
+        return random.choice(self.puzzles_by_rating[normalized_rating])
+
 
             
