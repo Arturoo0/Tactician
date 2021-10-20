9b17639787a1428cdb1466e81b3b6735b7526191
Arturoo0 <arturoportelles@gmail.com>
2021-10-20 13:57:20 -0400

    Remove puzzles from local storage

--- app/puzzles/util.py
+++ app/puzzles/util.py
@@ -4,25 +4,22 @@ import json
 
 class PuzzleContainer:
     def __init__(self):
-        mapped_by_id, mapped_by_rating = self.convert_csv_to_dict()
-        self.id_mapped_puzzles = mapped_by_id
-        self.puzzles_by_rating = mapped_by_rating
+        self.convert_csv_to_dict()
 
     def convert_csv_to_dict(self):
         id_mapped_puzzles = {}
-        puzzles_by_rating = [[] for arr in range(100)]
+        puzzles_by_rating = {}
         with open('app/puzzles/lichess_db_puzzle.csv') as f:
             reader = csv.DictReader(f)
             for row in reader:
                 rating = int(row['Rating'])
                 rounded_rating = rating - (rating % 100)
                 id_mapped_puzzles[row['PuzzleId']] = row
-                puzzles_by_rating[rounded_rating//100].append(row)
-        with open('app/puzzles/puzzle.json', 'w+') as fp:
-            json.dump(id_mapped_puzzles, fp)
+                puzzles_by_rating[rounded_rating//100] = (row)
+        # with open('app/puzzles/puzzle.json', 'w+') as fp:
+        #     json.dump(id_mapped_puzzles, fp)
         with open('app/puzzles/puzzle_ratings.json', 'w+') as fp:
-            json.dump({'puzzles_by_rating': puzzles_by_rating}, fp)
-        return (id_mapped_puzzles, puzzles_by_rating)
+            json.dump(puzzles_by_rating, fp)
 
     def pull_tactic(self, rating):
         normalized_rating = int((rating - (rating % 100))/100)
