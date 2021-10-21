506e246f3cc6b8f2b9c8b2c792cb49599caa984d
Arturoo0 <arturoportelles@gmail.com>
2021-10-21 19:14:26 -0400

    pull env port

--- run.py
+++ run.py
@@ -1,4 +1,6 @@
 from app import create_app
+import os
 
 application = create_app()
-application.run(debug=True, host="0.0.0.0")
+port = int(os.environ.get("PORT", 5000))
+application.run(debug=True, host="0.0.0.0", port=port)
