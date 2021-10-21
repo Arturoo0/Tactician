ce8b29ae41d55620cebb94ff9d87fb45f4879843
Arturoo0 <arturoportelles@gmail.com>
2021-10-21 19:49:06 -0400

    comment out prior code

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 from app import create_app
 import os
+application = create_app()
 
 # if __name__ == '__main__':
-application = create_app()
 # port = int(os.environ.get("PORT", 5000))
 # application.run(debug=True, host="0.0.0.0", port=port)
-# application.run()
