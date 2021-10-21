fec0019e43e9025d30b7b6e49b9cf328bf77f2a7
Arturoo0 <arturoportelles@gmail.com>
2021-10-21 19:25:46 -0400

    modify proc and run

--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 from app import create_app
 import os
 
-if __name__ == "__main__":
-    application = create_app()
-    port = int(os.environ.get("PORT", 5000))
-    application.run(debug=True, host="0.0.0.0", port=port)
+application = create_app()
+port = int(os.environ.get("PORT", 5000))
+application.run(debug=True, host="0.0.0.0", port=port)
