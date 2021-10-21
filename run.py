f754780a2eac236c5858d4cbc510c7dd6f706832
Arturoo0 <arturoportelles@gmail.com>
2021-10-21 19:40:48 -0400

    remove debug mode

--- run.py
+++ run.py
@@ -5,5 +5,4 @@ if __name__ == '__main__':
     application = create_app()
     # port = int(os.environ.get("PORT", 5000))
     # application.run(debug=True, host="0.0.0.0", port=port)
-    application.run(debug=True)
-    
+    application.run()
