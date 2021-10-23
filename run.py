cadba71fa5985e07b43c3aa5bd5f312744e03cb7
Arturoo0 <arturoportelles@gmail.com>
2021-10-23 10:54:54 -0400

    Change back run code

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
 from app import create_app
-import os
-application = create_app()
 
-# if __name__ == '__main__':
-# port = int(os.environ.get("PORT", 5000))
-# application.run(debug=True, host="0.0.0.0", port=port)
+if __name__ == '__main__':
+    application = create_app()
+    application.run(debug=True, host="0.0.0.0", port=port)
